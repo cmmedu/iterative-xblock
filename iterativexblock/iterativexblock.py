88c89fcd4ebad96c46e9353ad351c7186f4ff489
Vicente Daie Pinilla <vdaiep@gmail.com>
2024-02-20 01:06:19 -0300

    most of base functions

--- iterativexblock/iterativexblock.py
+++ iterativexblock/iterativexblock.py
@@ -62,12 +62,6 @@ class IterativeXBlock(XBlock):
         help="Wether to shown the download buttons or not."
     )
 
-    force_all_answers = Boolean(
-        default=False,
-        scope=Scope.settings,
-        help="Wether to shown the download buttons or not."
-    )
-
     content = Dict(
         default={},
         scope=Scope.settings,
@@ -126,7 +120,7 @@ class IterativeXBlock(XBlock):
 
 
     def studio_post_duplicate(self, store, source_item):
-        #pendiente
+        # pendiente
         from .models import IAAActivity, IAAStage
         if source_item.block_type == "full":
             item = IAAActivity.objects.last()
@@ -140,9 +134,6 @@ class IterativeXBlock(XBlock):
 
 
     def student_view(self, context={}):
-        from .models import IterativeXBlockQuestion, IterativeXBlockAnswer
-        from django.contrib.auth.models import User
-
         if getattr(self.runtime, 'user_is_staff', False):
             return self.instructor_view(self, context)
         else:
@@ -150,12 +141,10 @@ class IterativeXBlock(XBlock):
 
 
     def learner_view(self, context={}):
-        id_student = self.scope_ids.user_id
-        context.update(
-            {
-                "title": self.title
-            }
-        )
+        context = {
+            "title": self.title,
+            'location': str(self.location).split('@')[-1]
+        }
         template = loader.render_django_template(
             'public/html/iterativexblock_student.html',
             context=Context(context),
@@ -163,7 +152,7 @@ class IterativeXBlock(XBlock):
         )
         frag = self.build_fragment(
             template,
-            initialize_js_func='IterativeAssessedActivityStudent',
+            initialize_js_func='IterativeXBlockStudent',
             additional_css=[
                 'public/css/iterativexblock_{}.css'.format(self.style),
             ],
@@ -180,12 +169,11 @@ class IterativeXBlock(XBlock):
     
 
     def instructor_view(self, context={}):
-        id_student = self.scope_ids.user_id
-        context.update(
-            {
-                "title": self.title
-            }
-        )
+        from django.contrib.auth.models import User
+        context = {
+            "title": self.title,
+            'location': str(self.location).split('@')[-1]
+        }
         template = loader.render_django_template(
             'public/html/iterativexblock_student.html',
             context=Context(context),
@@ -193,7 +181,7 @@ class IterativeXBlock(XBlock):
         )
         frag = self.build_fragment(
             template,
-            initialize_js_func='IterativeAssessedActivityStudent',
+            initialize_js_func='IterativeXBlockStudent',
             additional_css=[
                 'public/css/iterativexblock_{}.css'.format(self.style),
             ],
@@ -210,16 +198,10 @@ class IterativeXBlock(XBlock):
 
 
     def studio_view(self, context):
-        from .models import IAAActivity, IAAStage
-        id_course = self.course_id
-        js_context = {
-            "title": self.title
+        context = {
+            "title": self.title,
+            'location': str(self.location).split('@')[-1]
         }
-        context.update(
-            {
-                "context": json.dumps(js_context)
-            }
-        )
         template = loader.render_django_template(
             'public/html/iterativexblock_studio.html',
             context=Context(context),
@@ -227,7 +209,7 @@ class IterativeXBlock(XBlock):
         )
         frag = self.build_fragment(
             template,
-            initialize_js_func='IterativeAssessedActivityStudio',
+            initialize_js_func='IterativeXBlockStudio',
             additional_js=[
                 'public/js/iterativexblock_studio.js',
             ],
@@ -236,85 +218,18 @@ class IterativeXBlock(XBlock):
 
 
     def author_view(self, context={}):
-        indicator_class = self.get_indicator_class()
-        if self.block_type == "none":
-            js_context = {
-                "block_type": self.block_type,
-                "title": self.title
-            }
-        elif self.block_type == "full":
-            from .models import IAAActivity, IAAStage
-            id_course = self.course_id
-            activity = IAAActivity.objects.filter(id_course=id_course, activity_name=self.activity_name).first()
-            stages_list = [[x["stage_number"], x["stage_label"]] for x in IAAStage.objects.filter(activity=activity).order_by("stage_number").values("stage_number", "stage_label")]
-            js_context = {
-                "title": self.title,
-                "activity_name": self.activity_name,
-                "block_type": self.block_type,
-                "activity_stage": self.activity_stage,
-                "stage_label": self.stage_label,
-                "question": self.question,
-                "min_length": self.min_length,
-                "stages": stages_list,
-                "activity_previous": self.activity_previous,
-                'location': str(self.location).split('@')[-1],
-                'indicator_class': indicator_class,
-            }
-            if self.activity_previous:
-                try:
-                    previous_activity = IAAActivity.objects.filter(id_course=id_course, activity_name=self.activity_name_previous).first()
-                    previous_stage = IAAStage.objects.get(activity=previous_activity, stage_number=self.activity_stage_previous)
-                    stage_label_previous = previous_stage.stage_label
-                    js_context["activity_name_previous"] = self.activity_name_previous
-                    js_context["activity_stage_previous"] = self.activity_stage_previous
-                    js_context["stage_label_previous"] = stage_label_previous
-                except:
-                    js_context["activity_name_previous"] = "ERROR"
-                    js_context["activity_stage_previous"] = "ERROR"
-                    js_context["stage_label_previous"] = "ERROR"
-        elif self.block_type == "display":
-            from .models import IAAActivity, IAAStage
-            id_course = self.course_id
-            js_context = {
-                "title": self.title,
-                "block_type": self.block_type,
-                'location': str(self.location).split('@')[-1],
-                'indicator_class': indicator_class,
-            }
-            try:
-                previous_activity = IAAActivity.objects.filter(id_course=id_course, activity_name=self.activity_name_previous).first()
-                previous_stage = IAAStage.objects.get(activity=previous_activity, stage_number=self.activity_stage_previous)
-                stage_label_previous = previous_stage.stage_label
-                js_context["activity_name_previous"] = self.activity_name_previous
-                js_context["activity_stage_previous"] = self.activity_stage_previous
-                js_context["stage_label_previous"] = stage_label_previous
-            except:
-                js_context["activity_name_previous"] = "ERROR"
-                js_context["activity_stage_previous"] = "ERROR"
-                js_context["stage_label_previous"] = "ERROR"
-        elif self.block_type == "summary":
-            js_context = {
-                "title": self.title,
-                "activity_name": self.activity_name,
-                "block_type": self.block_type,
-                "summary_text": self.summary_text,
-                "summary_list": self.summary_list,
-                'location': str(self.location).split('@')[-1],
-            }
-        else:
-            js_context = {
-                "title": self.title,
-                "block_type": self.block_type,
-                'location': str(self.location).split('@')[-1]
-            }
+        context = {
+            "title": self.title,
+            'location': str(self.location).split('@')[-1]
+        }
         template = loader.render_django_template(
             'public/html/iterativexblock_author.html',
-            context=Context(js_context),
+            context=Context(context),
             i18n_service=self.runtime.service(self, 'i18n'),
         )
         frag = self.build_fragment(
             template,
-            initialize_js_func='IterativeAssessedActivityAuthor',
+            initialize_js_func='IterativeXBlockAuthor',
             additional_css=[
                 'public/css/iterativexblock.css',
             ],
@@ -357,7 +272,10 @@ class IterativeXBlock(XBlock):
                 new_question.save()
             for question in deleted_questions:
                 # agregar id_xblock igual a este mismo
-                deleted_question = IterativeXBlockQuestion.objects.get(id_course=id_course, id_xblock=2, id_question=question.id_question)
+                try:
+                    deleted_question = IterativeXBlockQuestion.objects.get(id_course=id_course, id_xblock=2, id_question=question.id_question)
+                except IterativeXBlockQuestion.DoesNotExist:
+                    continue
                 deleted_answers = IterativeXBlockAnswer.objects.filter(id_course=id_course, question=deleted_question).all()
                 for answer in deleted_answers:
                     answer.delete()
@@ -367,20 +285,20 @@ class IterativeXBlock(XBlock):
         self.n_columns = data.get('n_columns')
         self.no_answer_message = data.get('no_answer_message')
         self.enable_download = data.get('enable_download')
-        self.force_all_answers = data.get('force_all_answers')
         return {'result': 'success'}
 
 
     @XBlock.json_handler
     def student_submit(self, data, suffix=''):
         """
+        Called when a student submits an answer to this XBlock.
         """
-        from .models import IAAActivity, IAAStage, IAASubmission
+        from .models import IterativeXBlockQuestion, IterativeXBlockAnswer
 
         id_course = self.course_id
         id_student = self.scope_ids.user_id
         if self.score != 0.0:
-            return {"result": 'repeated', "indicator_class": self.get_indicator_class()}
+            return {"result": 'repeated'}
         else:
             self.score = 1
             self.runtime.publish(
@@ -391,117 +309,36 @@ class IterativeXBlock(XBlock):
                     'max_value': 1
                 }
             )
-            current_activity = IAAActivity.objects.get(id_course=id_course, activity_name=self.activity_name)
-            current_stage = IAAStage.objects.get(activity=current_activity, stage_number=self.activity_stage)
-            new_submission_time = datetime.datetime.now()
-            new_submission = IAASubmission(stage=current_stage, id_student=id_student, submission=data["submission"], submission_time=new_submission_time)
-            new_submission.save()
-            return {"result": 'success', "indicator_class": self.get_indicator_class()}
+            submission_time = datetime.datetime.now()
+            for answer in data["answers"]:
+                # agregar id_xblock
+                try:
+                    question = IterativeXBlockQuestion.objects.get(id_course=id_course, id_xblock=2, id_question=answer["id_question"])
+                except IterativeXBlockQuestion.DoesNotExist:
+                    # manejar este caso
+                    continue
+                new_answer = IterativeXBlockAnswer(question=question, id_course=id_course, id_student=id_student, answer=answer["answer"], timestamp=submission_time)
+                new_answer.save()
+            return {"result": 'success'}
 
     
     @XBlock.json_handler
     def fetch_previous_submission(self, data, suffix=''):
         """
+        Called when a student wants to see their previous submission.
         """
-        from .models import IAAActivity, IAAStage, IAASubmission
+        from .models import IterativeXBlockQuestion, IterativeXBlockAnswer
 
         id_course = self.course_id
         id_student = self.scope_ids.user_id
-        current_activity_previous = IAAActivity.objects.get(id_course=id_course, activity_name=self.activity_name_previous)
-        current_stage_previous = IAAStage.objects.get(activity=current_activity_previous, stage_number=self.activity_stage_previous)
-        current_submission_previous = IAASubmission.objects.filter(stage=current_stage_previous, id_student=id_student).values("submission", "submission_time")
+        id_question = data["id_question"]
+        # agregar id_xblock
         try:
-            current_activity_previous = IAAActivity.objects.get(id_course=id_course, activity_name=self.activity_name_previous)
-            current_stage_previous = IAAStage.objects.get(activity=current_activity_previous, stage_number=self.activity_stage_previous)
-            stage_label_previous = current_stage_previous.stage_label
-            current_submission_previous = IAASubmission.objects.filter(stage=current_stage_previous, id_student=id_student).values("submission", "submission_time")
-            if len(current_submission_previous) == 0:
-                this_submission_previous = "EMPTY"
-                this_submission_time_previous = "EMPTY"
-            else:
-                this_submission_previous = current_submission_previous[0]["submission"].replace("&", "&amp;").replace(">", "&gt;").replace("<", "&lt;")
-                this_submission_time_previous = str(current_submission_previous[0]["submission_time"])
-        except:
-            this_submission_previous = "ERROR"
-            this_submission_time_previous = "ERROR"
-            stage_label_previous = "ERROR"
-        return {"result": 'success', 'submission_previous': this_submission_previous, 'submission_previous_time': this_submission_time_previous, "stage_label_previous": stage_label_previous, "indicator_class": self.get_indicator_class()}
-
-
-    @XBlock.json_handler
-    def fetch_summary(self, data, suffix=''):
-        """
-        """
-        from .models import IAAActivity, IAAStage, IAASubmission
-        from django.contrib.auth.models import User
-
-        enrolled = User.objects.filter(courseenrollment__course_id=self.course_id,courseenrollment__is_active=1).order_by('id').values('id' ,'first_name', 'last_name', 'email')
-        id_course = self.course_id
-        if data["user_id"] == "self":
-            id_student = self.scope_ids.user_id
-        else:
-            id_student = data["user_id"]
-        name = None
-        for x in enrolled:
-            if x["id"] == id_student:
-                name = x["first_name"] + " " + x["last_name"]
-                break
+            question = IterativeXBlockQuestion.objects.get(id_course=id_course, id_xblock=2, id_question=id_question)
+        except IterativeXBlockQuestion.DoesNotExist:
+            return {"result": 'failed', 'error': 501}
         try:
-            current_activity = IAAActivity.objects.get(id_course=id_course, activity_name=self.activity_name)
-            summary = []
-            stages_list = IAAStage.objects.filter(activity=current_activity).order_by("stage_number").all()
-            for stage in stages_list:
-                if stage.stage_number in self.summary_list.split(","):
-                    submission = IAASubmission.objects.filter(stage=stage, id_student=id_student).values("submission", "submission_time")
-                    if len(submission) == 0:
-                        this_summary_submission = "No se registra respuesta."
-                        this_summary_submission_time = "—"
-                    else:
-                        this_summary_submission = submission[0]["submission"].replace("&", "&amp;").replace(">", "&gt;").replace("<", "&lt;")
-                        this_summary_submission_time = str(submission[0]["submission_time"])
-                    summary.append((stage.stage_number, stage.stage_label, this_summary_submission, this_summary_submission_time))
-            return {"result": "success", "summary": summary, "indicator_class": self.get_indicator_class(), "name": name, "is_summary": self.block_type == "summary"}
-        except:
-            return {"result": "failed", "indicator_class": self.get_indicator_class()}
-
-
-    @XBlock.json_handler
-    def instructor_submit(self, data, suffix=''):
-        """
-        """
-        from .models import IAAActivity, IAAStage, IAAFeedback
-        id_course = self.course_id
-        id_instructor = self.scope_ids.user_id
-        current_activity = IAAActivity.objects.get(id_course=id_course, activity_name=self.activity_name)
-        current_stage = IAAStage.objects.get(activity=current_activity)
-        id_student = data.get("id_student")
-        feedback = data.get("feedback")
-        new_feedback_time = datetime.datetime.now()
-        existing_feedback = IAAFeedback.objects.filter(stage=current_stage, id_instructor=id_instructor, id_student=id_student).all()
-        if len(existing_feedback) == 0:
-            new_feedback = IAAFeedback(stage=current_stage, id_instructor=id_instructor, id_student=id_student, feedback=feedback, feedback_time=new_feedback_time)
-            new_feedback.save()
-        else:
-            existing_feedback = IAAFeedback.objects.get(stage=current_stage, id_instructor=id_instructor, id_student=id_student)
-            existing_feedback.feedback = feedback
-            existing_feedback.feedback_time = new_feedback_time
-            existing_feedback.save()
-        return {"result": "success"}
-
-
-    def get_indicator_class(self):
-        indicator_class = 'unanswered'
-        if self.score != 0:
-            indicator_class = 'correct'
-        return indicator_class
-
-
-    @staticmethod
-    def workbench_scenarios():
-        """A canned scenario for display in the workbench."""
-        return [
-            ("IterativeAssessedActivityXBlock",
-             """<iterativexblock/>
-             """)
-        ]
-
+            answer = IterativeXBlockAnswer.objects.get(id_course=id_course, question=question, id_student=id_student)
+        except IterativeXBlockAnswer.DoesNotExist:
+            return {"result": 'failed', 'error': 502}
+        return {"result": 'success', 'answer': answer.answer, 'answer_time': str(answer.timestamp)}
