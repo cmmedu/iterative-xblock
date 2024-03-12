e98b5c3f182a6a3049fdf38e3c3b8b2095370ce0
Vicente Daie Pinilla <vdaiep@gmail.com>
2024-03-12 14:33:52 -0300

    instructor view

--- iterativexblock/public/js/iterativexblock_instructor.js
+++ iterativexblock/public/js/iterativexblock_instructor.js
@@ -9,6 +9,8 @@ function IterativeXBlockInstructor(runtime, element, settings) {
         $(element).find(".iterative-xblock-student-question").val("");
         $(element).find(".iterative-xblock-student-get-answer").show();
         $(element).find(".iterative-xblock-student-answer").val("");
+        console.log(selectedUser)
+        console.log(answers)
         if (selectedUser !== "none") {
             let selectedUserAnswers = answers.find(obj => obj.id_student === selectedUser);
             for (var key in selectedUserAnswers.answers) {
